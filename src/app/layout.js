c5eb7cc8502230bd99059b2b15c5c321fd5383c0
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-09-21 16:04:11 +0300

    🔨 refactor(layout.js): optimize font imports and use variables for font names and weights 🔨 refactor(layout.js): update HTML structure and add class names for font variables 🔨 refactor(page.js): remove unnecessary imports and simplify component structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,32 @@
 import './globals.css';
 
+import { Bangers, Quicksand, Roboto_Condensed } from 'next/font/google';
+
+const quicksand = Quicksand({
+  subsets: ['latin'],
+  variable: '--font-quicksand'
+});
+
+const bangers = Bangers({
+  subsets: ['latin'],
+  variable: '--font-bangers',
+  weight: ['400']
+});
+
+const roboto_condensed = Roboto_Condensed({
+  subsets: ['latin'],
+  variable: '--font-roboto-condensed',
+  weight: ['300', '400', '700']
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang="en">
+      <body
+        className={`${quicksand.variable} ${bangers.variable} ${roboto_condensed.variable} font-quicksand`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
