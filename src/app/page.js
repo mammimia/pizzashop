dc50ac22a14a38b660483e02af40e147e4ba179e
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-09-21 22:46:10 +0300

    🎉 feat(Banner.js): add new Banner component to display a visually appealing banner on the homepage 🔀 chore(page.js): import and render the new Banner component on the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Banner from './components/Banner';
 import Pizza from './components/Pizza';
 import { pizzas } from './pizzas';
 
 export default function Home() {
   return (
     <section>
+      <Banner />
       <div className="container mx-auto">
         <div className="grid grid-cols-2 gap-[15px] md:grid-cols-3 xl:grid-cols-4 xl:gap-[30px] py-12">
           {pizzas.map((pizza) => {
