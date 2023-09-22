631a953a01f2bd95dde2ea0b3bd673c5cef18000
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-09-22 22:52:08 +0300

    📦 feat(CartMobileIcon.js): add CartMobileIcon component to display a mobile cart icon with a badge showing the number of items in the cart 🐛 fix(CartContext.js): set initial value of isOpen state to true instead of false to ensure the cart is initially open

--- src/app/components/cart/CartMobileIcon.js
+++ src/app/components/cart/CartMobileIcon.js
@@ -1,12 +1,16 @@
-import React from 'react';
+'use client';
+import React, { useContext } from 'react';
 import { BsHandbagFill } from 'react-icons/bs';
+import { CartContext } from '../../context/CartContext';
 
 const CartMobileIcon = () => {
+  const { isOpen, setIsOpen } = useContext(CartContext);
   return (
     <div
       className="bg-tertiary w-[72px] h-[72px] rounded-full flex 
     justify-center items-center text-white cursor-pointer fixed right-[10%]
      bottom-[5%] z-20 lg:hidden"
+      onClick={() => setIsOpen(!isOpen)}
     >
       <BsHandbagFill className="text-4xl" />
       <span
--- src/app/context/CartContext.js
+++ src/app/context/CartContext.js
@@ -5,7 +5,7 @@ import react, { createContext, useState } from 'react';
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <CartContext.Provider value={{ isOpen, setIsOpen }}>
       {children}
