7cbdd9357cf476d42d590cf581c8c38fafe0720f
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-09-22 23:26:10 +0300

    🔧 fix(CartContext.js): fix initial value of isOpen state to be false instead of true for better default behavior ✨ feat(CartContext.js): add cartItems state and functions addToCart and removeFromCart to manage items in the cart

--- src/app/context/CartContext.js
+++ src/app/context/CartContext.js
@@ -6,8 +6,29 @@ export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [isOpen, setIsOpen] = useState(true);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    const newItem = {
+      ...item,
+      additionalToppings: item.additionalToppings.sort((a, b) =>
+        a.name.localeCompare(b.name)
+      )
+    };
+    setCartItems([...cartItems, newItem]);
+  };
+
+  const removeFromCart = (item) => {
+    const newCartItems = cartItems.filter(
+      (cartItem) => cartItem.id !== item.id
+    );
+    setCartItems(newCartItems);
+  };
+
   return (
-    <CartContext.Provider value={{ isOpen, setIsOpen }}>
+    <CartContext.Provider
+      value={{ isOpen, setIsOpen, cartItems, addToCart, removeFromCart }}
+    >
       {children}
     </CartContext.Provider>
   );
