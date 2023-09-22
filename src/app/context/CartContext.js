6b1b871d75043cbbcdb923d5e6416370738bdbec
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-09-22 22:42:28 +0300

    📦 chore(CartContext.js): add CartContext to manage cart state and provide it to components 🔀 merge(layout.js): integrate CartProvider into RootLayout to provide CartContext to components

---
+++ src/app/context/CartContext.js
@@ -0,0 +1,16 @@
+'use client';
+
+import react, { createContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <CartContext.Provider value={{ isOpen, setIsOpen }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import CartMobileIcon from './components/CartMobileIcon';
 import Nav from './components/Nav';
+import CartProvider from './context/CartContext';
 import './globals.css';
 
 import { Bangers, Quicksand, Roboto_Condensed } from 'next/font/google';
@@ -23,14 +24,16 @@ const roboto_condensed = Roboto_Condensed({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${quicksand.variable} ${bangers.variable} ${roboto_condensed.variable} font-quicksand`}
-      >
-        <Nav />
-        <CartMobileIcon />
-        {children}
-      </body>
-    </html>
+    <CartProvider>
+      <html lang="en">
+        <body
+          className={`${quicksand.variable} ${bangers.variable} ${roboto_condensed.variable} font-quicksand`}
+        >
+          <Nav />
+          <CartMobileIcon />
+          {children}
+        </body>
+      </html>
+    </CartProvider>
   );
 }
