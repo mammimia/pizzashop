2fbbf7714715cf87915cb321a86feff577332353
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-09-24 13:06:26 +0300

    ✨ feat(SuccessMessage.js): create SuccessMessage component to display a success message after placing an order and automatically close the window after a countdown 🐛 fix(SuccessMessage.js): fix linting issues and formatting for better code readability

---
+++ src/app/components/checkout/SuccessMessage.js
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+
+const SuccessMessage = ({ setCartItems, closeModal, setSuccessMessage }) => {
+  const [count, setCount] = useState(5);
+
+  useEffect(() => {
+    if (count > 0) {
+      setTimeout(() => {
+        setCount(count - 1);
+      }, 1000);
+    } else {
+      closeModal();
+      setSuccessMessage(false);
+      setCartItems([]);
+    }
+  }, [setSuccessMessage, count, setCartItems, closeModal]);
+
+  return (
+    <div
+      className="flex flex-col justify-center items-center h-[100vh] lg:h-[600px]
+  px-6"
+    >
+      <h2 className="text-2xl font-semibold text-center">
+        Thank you! The order has been placed!
+      </h2>
+      <Image src="/success-1.gif" alt="" width={150} height={150} />
+      <div>
+        This window will close automatically in <span>{count}</span> seconds.
+      </div>
+    </div>
+  );
+};
+
+export default SuccessMessage;
