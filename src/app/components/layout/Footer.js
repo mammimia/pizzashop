b8fa4ec6c15fcdce99de5d09bc5d586e11339a91
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-09-24 13:18:54 +0300

    🎨 style(Footer.js): improve footer layout and design 🔀 chore(Footer.js): add missing imports for next/image and react-icons/fa 🔀 chore(Footer.js): add missing Link component import from next/link 🔀 chore(Footer.js): remove unused import for React 🔀 chore(Footer.js): remove unused return statement 🔀 chore(Footer.js): add missing alt attribute to Image component 🔀 chore(Footer.js): add missing container and mx-auto classes for centering content 🔀 chore(Footer.js): add missing flex classes for aligning and spacing elements 🔀 chore(Footer.js): add missing gap-x-6 class for spacing social media icons 🔀 chore(Footer.js): add missing text-xl class for social media icons 🔀 chore(Footer.js): add missing font-medium class for copyright text 🔀 chore(Footer.js): update copyright year to 2023

--- src/app/components/layout/Footer.js
+++ src/app/components/layout/Footer.js
@@ -1,7 +1,42 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaPinterest,
+  FaYoutube
+} from 'react-icons/fa';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary bg-pattern py-16">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center gap-y-6 justify-center">
+          <Link href="#">
+            <Image src="logo.svg" width={180} height={180} alt="" />
+          </Link>
+          <div className="flex gap-x-6 text-xl text-white">
+            <Link href="#">
+              <FaYoutube />
+            </Link>
+            <Link href="#">
+              <FaFacebook />
+            </Link>
+            <Link href="#">
+              <FaInstagram />
+            </Link>
+            <Link href="#">
+              <FaPinterest />
+            </Link>
+          </div>
+          <div className="text-white font-medium">
+            Copyright &copy; Pizzaland 2023. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
